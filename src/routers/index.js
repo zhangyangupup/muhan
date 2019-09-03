2c07e56537918c1e87b14f1a26475555f0b21c2c
21870-杜彦丽 <yanli.du01@hand-china.com>
2019-09-03 21:50:36 +0800

    test

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,32 +1,35 @@
-let routes = [
-  {
-    path: '/main',
-    name: 'main',
-    component: () => import('../views/main/index.vue'),
-    children: [
-      {
-        path: '',
-        component: () => import('../views/main/main-home/index.vue')
-      },
-      {
-        path: 'community',
-        component: () => import('../views/main/community/index.vue')
-      },
-      {
-        path: 'personal',
-        component: () => import('../views/main/applications/index.vue')
-      },
-      {
-        path: 'applications',
-        component: () => import('../views/main/personal/index.vue')
-      }
-    ]
+let routes = [{
+  path: '/',
+  name: 'login',
+  component: () => import('../views/login/index.vue')
+},
+{
+  path: '/main',
+  name: 'main',
+  component: () => import('../views/main/index.vue'),
+  children: [{
+    path: '',
+    component: () => import('../views/main/main-home/index.vue')
   },
   {
-    path: 'search',
-    name: 'SearchPage',
-    component: () => import('../views/search-page/index.vue')
+    path: 'community',
+    component: () => import('../views/main/community/index.vue')
+  },
+  {
+    path: 'personal',
+    component: () => import('../views/main/applications/index.vue')
+  },
+  {
+    path: 'applications',
+    component: () => import('../views/main/personal/index.vue')
   }
+  ]
+},
+{
+  path: 'search',
+  name: 'SearchPage',
+  component: () => import('../views/search-page/index.vue')
+}
 ]
 
 export default routes
---
+++ src/views/login/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="login">
+    <div slot="header">
+      <div class="header-box"></div>
+    </div>
+    <div slot="body">
+      <div class="body-box">
+        <p>登录</p>
+        <div class="user-box"></div>
+        <div class="pass-box"></div>
+      </div>
+    </div>
+    <div slot="footer">
+      <div class="footer-box">
+        <button></button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import button from '../../components/ui-pc/button/button'
+export default {
+  components: {
+    button
+  }
+}
+</script>
+
+<style lang="stylus">
+.body-box
+  background-color baseColor
+  width 100%
+  height 40px
+</style>
